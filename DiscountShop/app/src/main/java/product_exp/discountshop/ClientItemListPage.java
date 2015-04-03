f6036cd47e24096ff399d12048cce7045dc07280
Chih-Feng Lin <phone751230@gmail.com>
2015-04-03 12:41:08 -0700

    create dynamic list view

--- DiscountShop/app/src/main/java/product_exp/discountshop/ClientItemListPage.java
+++ DiscountShop/app/src/main/java/product_exp/discountshop/ClientItemListPage.java
@@ -1,19 +1,18 @@
 package product_exp.discountshop;
 
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class ItemListPage extends ListActivity {
-
+public class ClientItemListPage extends ListActivity{
     private MyAdapter myAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list_page);
+        setContentView(R.layout.list);
         myAdapter = new MyAdapter(this);
         setListAdapter(myAdapter);
     }
