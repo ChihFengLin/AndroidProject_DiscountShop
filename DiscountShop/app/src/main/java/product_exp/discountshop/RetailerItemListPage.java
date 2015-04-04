7fbce06fbf32a3247f8871f2581517b5046df668
Chih-Feng Lin <phone751230@gmail.com>
2015-04-03 17:09:14 -0700

    create retailer item list page

--- DiscountShop/app/src/main/java/product_exp/discountshop/RetailerItemListPage.java
+++ DiscountShop/app/src/main/java/product_exp/discountshop/RetailerItemListPage.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ClientItemListPage extends ListActivity{
+
+public class RetailerItemListPage extends ListActivity {
+
     private MyAdapter myAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list);
+        setContentView(R.layout.activity_item_list_page);
         myAdapter = new MyAdapter(this);
         setListAdapter(myAdapter);
     }
@@ -44,30 +46,4 @@ public class ClientItemListPage extends ListActivity{
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /*
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_item_list_page, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    */
 }
